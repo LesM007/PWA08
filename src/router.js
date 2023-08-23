ac45cd2651f06e414ca21c4f5962e282bfcf76f7
LesM007 <U1501857@RTS-365.dk>
2023-08-23 11:34:45 +0200

    OneSignal commit

--- src/router.js
+++ src/router.js
@@ -6,12 +6,14 @@ import {
 import App from "./App";
 import Home from "./pages/Home";
 import Location from "./pages/Location";
+import Geolocation from "./pages/Geolocation";
 
 export const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route index element={<Home />} />
       <Route path="/location" element={<Location />} />
+      <Route path="/geolocation" element={<Geolocation />} />
     </Route>
   )
 );
