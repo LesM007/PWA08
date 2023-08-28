806f8d57c2ac68149b21dfbcee6faf829969456d
LesM007 <U1501857@RTS-365.dk>
2023-08-28 09:19:01 +0200

    iPhone support gyro commit

--- src/pages/Gyro.js
+++ src/pages/Gyro.js
@@ -1,9 +1,17 @@
 import { useState, useEffect } from "react";
+import { enable } from "workbox-navigation-preload";
 
 const Gyro = () => {
   const [deviceAlpha, setDeviceAlpha] = useState();
   const [deviceBeta, setDeviceBeta] = useState();
   const [deviceGamma, setDeviceGamma] = useState();
+  const [isIOS, setIsIOS] = useState(false);
+
+  useEffect(() => {
+    if (navigator.userAgent.match(/iPhone/i)) {
+      setIsIOS(true);
+    }
+  }, []);
 
   useEffect(() => {
     window.addEventListener("deviceorientation", (e) => {
@@ -13,13 +21,30 @@ const Gyro = () => {
     });
   }, []);
 
+  const enableDeviceOrientation = () => {
+    DeviceOrientationEvent.requestPermission().then((response) => {
+      if (response === "granted") {
+        window.addEventListener("deviceorientation", (e) => {
+          setDeviceAlpha(e.alpha.toFixed(1));
+          setDeviceBeta(e.beta.toFixed(1));
+          setDeviceGamma(e.gamma.toFixed(1));
+        });
+      }
+    });
+  };
+
   return (
     <>
       <h1>Gyroskop</h1>
+      {isIOS && <p>Du er på en iPhone!</p>}
       <h2>Værdier fra gyroskopet:</h2>
       <p>alpha:{deviceAlpha}</p>
       <p>beta:{deviceBeta}</p>
       <p>gamma:{deviceGamma}</p>
+
+      {isIOS && !deviceAlpha ? (
+        <button onClick={enableDeviceOrientation}>Enable on iPhone</button>
+      ) : null}
     </>
   );
 };
