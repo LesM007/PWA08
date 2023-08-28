8ef1e257f6fc1287e0be6210468247b0f86f46cb
LesM007 <U1501857@RTS-365.dk>
2023-08-28 12:18:21 +0200

    localbase/indeexed commit

---
+++ src/pages/Localbs.js
@@ -0,0 +1,36 @@
+import Localbase from "localbase";
+
+const Localbs = () => {
+  let myDatabase = new Localbase("myDatabase");
+
+  const insertIntoDB = () => {
+    myDatabase.collection("users").add({
+      id: 5232534,
+      name: "Bo",
+      age: 27,
+    });
+  };
+
+  const getFromDB = () => {
+    myDatabase
+      .collection("users")
+      .get()
+      .then((users) => console.log(users));
+  };
+
+  const removeCollection = () => {
+    myDatabase.collection("users").delete();
+  };
+
+  return (
+    <>
+      <button onClick={insertIntoDB}>Tilføj en bruger til databasen</button>
+      <button onClick={getFromDB}>Hent alle brugere fra databasen</button>
+      <button onClick={removeCollection}>
+        Slet alle brugere fra databasen
+      </button>
+    </>
+  );
+};
+
+export default Localbs;
